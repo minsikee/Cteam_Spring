334a16bcbd2e094758983506a893d05af9d011a5
greatjj <nojobghost@gmail.com>
2020-08-18 11:40:02 +0900

    d

---
+++ AnSpring_Cteam/src/main/java/com/cteam/app/controller/SuanController.java
@@ -0,0 +1,34 @@
+package com.cteam.app.controller;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import org.jcodec.api.FrameGrab;
+import org.jcodec.api.JCodecException;
+import org.jcodec.common.model.Picture;
+import org.jcodec.scale.AWTUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
+
+import com.cteam.app.command.AnCommand;
+
+@Controller
+public class SuanController {
+	
+	AnCommand command;
+	
+	
+
+}
